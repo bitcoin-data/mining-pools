1440ff1f596c3c9329075f60437387a99f955401
0xb10c <b10c@b10c.me>
2023-02-20 18:49:57 +0100

    gen-old-pools: arg parser, allow specifying dir

--- contrib/generate-old-pools-json.py
+++ contrib/generate-old-pools-json.py
@@ -1,38 +1,47 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import glob
 import sys
 
-output_file_name = "pools.json"
+DEFAULT_ENTITIES_DIR = "pools/"
+DEFAULT_OUTPUT = "pools.json"
 
-if len(sys.argv) == 2:
-    print(f"Using {sys.argv[1]} as output file name.")
-    output_file_name = sys.argv[1]
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("output", default=DEFAULT_OUTPUT, type=str, help="output file name", nargs="?")
+    parser.add_argument("entities", default=DEFAULT_ENTITIES_DIR, type=str, help="entities directory", nargs="?")
+    args = parser.parse_args(args)
 
-entity_files = glob.glob("pools/*.json")
+    if args.output != DEFAULT_OUTPUT:
+        print(f"Using {args.output} as output file name.")
 
-addresses = dict()
-tags = dict()
+    entity_files = glob.glob(args.entities + "/*.json")
 
-for file_path in entity_files:
-    with open(file_path, "r") as f:
-        e = json.load(f)
-        name = e["name"]
-        link = e["link"]
-        for addr in e["addresses"]:
-            addresses[addr] = { "name": name, "link": link }
-        for tag in e["tags"]:
-            tags[tag] = { "name": name, "link": link }
+    addresses = dict()
+    tags = dict()
 
-# sort dicts to be at least somewhat deterministic
-addresses = dict(sorted(addresses.items()))
-tags = dict(sorted(tags.items()))
+    for file_path in entity_files:
+        with open(file_path, "r") as f:
+            e = json.load(f)
+            name = e["name"]
+            link = e["link"]
+            for addr in e["addresses"]:
+                addresses[addr] = { "name": name, "link": link }
+            for tag in e["tags"]:
+                tags[tag] = { "name": name, "link": link }
 
-with open(output_file_name, "w") as out:
-    content = {
-        "payout_addresses": addresses,
-        "coinbase_tags": tags
-    }
-    json.dump(content, out, indent = 2, ensure_ascii=False)
+    # sort dicts to be at least somewhat deterministic
+    addresses = dict(sorted(addresses.items()))
+    tags = dict(sorted(tags.items()))
 
+    with open(args.output, "w") as out:
+        content = {
+            "payout_addresses": addresses,
+            "coinbase_tags": tags
+        }
+        json.dump(content, out, indent = 2, ensure_ascii=False)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
